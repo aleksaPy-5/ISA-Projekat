abc6f003cb883292a1fffa7a3a4b18f9476d01da
aleksaPy-5 <aleksandar.adzic.23@singimail.rs>
2026-06-22 17:58:44 +0200

    KategorijaController
    Dodavanje KategroijaControllera

--- projekat/src/main/java/rs/singidunum/projekat/controller/KategorijaController.java
+++ projekat/src/main/java/rs/singidunum/projekat/controller/KategorijaController.java
@@ -1,5 +1,52 @@
 package rs.singidunum.projekat.controller;
 
-public class KategorijaController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import rs.singidunum.projekat.model.Kategorija;
+import rs.singidunum.projekat.service.KategorijaService;
+
+@RestController
+@RequestMapping("/kategorije")
+public class KategorijaController {
+	
+	private final KategorijaService kategorijaService;
+	
+	public KategorijaController(KategorijaService kategorijaService) {
+        this.kategorijaService = kategorijaService;
+    }
+
+    @GetMapping
+    public List<Kategorija> findAll() {
+        return kategorijaService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Kategorija findById(@PathVariable Long id) {
+        return kategorijaService.findById(id);
+    }
+
+    @PostMapping
+    public Kategorija save(@RequestBody Kategorija kategorija) {
+        return kategorijaService.save(kategorija);
+    }
+
+    @PutMapping("/{id}")
+    public Kategorija update(@PathVariable Long id,
+                             @RequestBody Kategorija kategorija) {
+        return kategorijaService.update(id, kategorija);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        kategorijaService.delete(id);
+    }
 }
