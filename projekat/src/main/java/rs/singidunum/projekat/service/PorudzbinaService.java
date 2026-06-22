8a7b70d5d81c5bb7dcc9dff4457691447f346750
aleksaPy-5 <aleksandar.adzic.23@singimail.rs>
2026-06-22 17:25:17 +0200

    PorudzbinaService
    Dodavanje PorudzbinaService

--- projekat/src/main/java/rs/singidunum/projekat/service/PorudzbinaService.java
+++ projekat/src/main/java/rs/singidunum/projekat/service/PorudzbinaService.java
@@ -1,5 +1,33 @@
 package rs.singidunum.projekat.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import rs.singidunum.projekat.model.Porudzbina;
+import rs.singidunum.projekat.repository.PorudzbinaRepository;
+
+
+@Service
 public class PorudzbinaService {
 
+
+    private final PorudzbinaRepository porudzbinaRepository;
+	
+	public PorudzbinaService(PorudzbinaRepository porudzbinaRepository) {
+		this.porudzbinaRepository = porudzbinaRepository;
+
+	}
+	
+	public List <Porudzbina> findAll() {
+		return porudzbinaRepository.findAll();
+	}
+	
+	public Porudzbina findById(Long id) {
+		return porudzbinaRepository.findById(id).orElseThrow(()-> new RuntimeException("Poruzbina ne postoji"));
+	}
+	
+	public Porudzbina save(Porudzbina porudzbina) {
+		return porudzbinaRepository.save(porudzbina);
+	}
 }
