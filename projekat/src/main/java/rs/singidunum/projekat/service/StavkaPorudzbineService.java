fb750f0e584c7e1942efef544ae9adb91af2da54
aleksaPy-5 <aleksandar.adzic.23@singimail.rs>
2026-06-22 17:12:14 +0200

    StavkaPorudzbineService
    Dodavanje StavkaPorudzbineService

--- projekat/src/main/java/rs/singidunum/projekat/service/StavkaPorudzbineService.java
+++ projekat/src/main/java/rs/singidunum/projekat/service/StavkaPorudzbineService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import rs.singidunum.projekat.model.StavkaPorudzbine;
 import rs.singidunum.projekat.repository.StavkaPorudzbineRepository;
 
 @Service
@@ -16,7 +17,29 @@ public class StavkaPorudzbineService {
 	}
 	
 	public List <StavkaPorudzbine> findAll() {
-		
+		return stavkaPorudzbineRepository.findAll();
+	}
+	
+	public StavkaPorudzbine findById(Long id) {
+		return stavkaPorudzbineRepository.findById(id).orElseThrow(()-> new RuntimeException("Stavka porudzbine ne postoji"));
+	}
+	
+	public StavkaPorudzbine save(StavkaPorudzbine stavkaPorudzbine) {
+		return stavkaPorudzbineRepository.save(stavkaPorudzbine);
+	}
+	
+	public StavkaPorudzbine update(Long id, StavkaPorudzbine izmeniStavkaPorudzbine) {
+	    StavkaPorudzbine postojecaStavkaPorudzbine = findById(id);
+
+	    postojecaStavkaPorudzbine.setKolicina(izmeniStavkaPorudzbine.getKolicina());
+	    postojecaStavkaPorudzbine.setCena(izmeniStavkaPorudzbine.getCena());
+
+	    return stavkaPorudzbineRepository.save(postojecaStavkaPorudzbine);
+	}
+	
+	public void delete(Long id) {
+		StavkaPorudzbine stavkaPorudzbine = findById(id);
+		stavkaPorudzbineRepository.delete(stavkaPorudzbine);
 	}
 
 }
